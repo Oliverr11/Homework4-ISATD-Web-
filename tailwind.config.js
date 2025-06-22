2c9652d0a1e32c3c531161e1a7e29238edc2ab23
Oliverr11 <mrrsaki0403@gmail.com>
2025-06-22 21:45:05 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./html/**/*.html"], // adjust to your project
+  theme: {
+    extend: {
+      fontFamily: {
+        kanTumRuy: ["Kantumruy Pro", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
